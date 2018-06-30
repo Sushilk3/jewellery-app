31cac7f5c5222a4f3705e79dd61137a47b7fd15b
Chetna2906 <chtnswrnkr1@gmail.com>
2018-06-30 17:50:45 +0530

    Jewellery>>utility>>model>>commit

---
+++ src/main/java/com/web/model/Utility.java
@@ -0,0 +1,60 @@
+package com.web.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "utility")
+public class Utility {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "type")
+	private String type;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "details")
+	private String details;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+}
